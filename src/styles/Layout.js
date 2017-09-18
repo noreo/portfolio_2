2c31dbc1c5968e8577b50bbfc47cd1979e101d95
noreo <abercon@gmail.com>
2017-09-18 16:48:39 -0400

    added global font (avenir)
    added font files
    +css on global

--- src/styles/Layout.js
+++ src/styles/Layout.js
@@ -2,10 +2,20 @@ import styled, { injectGlobal } from "styled-components";
 import colors from './Colors';
 import React from 'react';
 import {withRouter} from 'react-router-dom';
+import path_avenir_medium from './fonts/Avenir/Avenir-Medium.ttf';
 
 const transition = '0.2s cubic-bezier(.33,.44,.29,.75)';
 
+
+
 injectGlobal`
+@font-face {
+  font-family: 'Avenir-Medium';
+  src: url(${path_avenir_medium}) format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
   html,
   body{ 
     margin: 0;
@@ -14,7 +24,7 @@ injectGlobal`
 
   body {
 	  font-size: 16px;
-	  font-family: 'arial', sans-serif;
+	  font-family: 'Avenir-Medium',  serif;
     /*background: ${colors.deepspace};*/
   }
   *:hover{
